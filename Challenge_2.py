f87f7d70910feb3d7d01236c193575b41c3c47cc
SumayaNihon <141727250+SumayaNihon@users.noreply.github.com>
2023-08-11 15:46:00 +0530

    Delete Challenge_2.py

--- Challenge_2.py
+++
@@ -1,9 +0,0 @@
-def extractthemiddlecharacters(word):
-    if (len(word)%2 == 0):
-        print(f"The given string {word} is even .\nExtracting the middle two characters : {word[(len(word)-1)//2:(len(word)+2)//2]}")
-
-    else:
-        print(f"The given string {word} is odd .\nExtracting the middle character : {word[(len(word)-1)//2:(len(word)+2)//2]}")
-
-extractthemiddlecharacters(input())
-
