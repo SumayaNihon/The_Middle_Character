332302821164d530b37a29a78a9c8145f5af1f14
SumayaNihon <141727250+SumayaNihon@users.noreply.github.com>
2023-08-11 10:12:13 +0000

    Refactoring the Code

---
+++ Extracting_Middle_Characters.py
@@ -0,0 +1,9 @@
+def extractmiddlecharacters(word):
+    if (len(word)%2 == 0):
+        print(f"The given string {word} is even .\nExtracting the middle two characters : {word[(len(word)-1)//2:(len(word)+2)//2]}")
+
+    else:
+        print(f"The given string {word} is odd .\nExtracting the middle character : {word[(len(word)-1)//2:(len(word)+2)//2]}")
+
+extractmiddlecharacters(input())
+
